f508ba413b01851b9eed24722ab4fa224d4ca0bf
Marker31NC <78875115+Marker31NC@users.noreply.github.com>
2021-11-08 20:49:18 -0500

    Add files via upload

---
+++ JS/app3.js
@@ -0,0 +1,47 @@
+const navBar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+
+//Builds Navbar with anchors
+for (let i = 0; i < sections.length; i++) {
+    let navElement = document.createElement("li");
+    navElement.innerHTML = "<a class = 'menu__link' href = ''>" + sections[i].getAttribute("data-nav") + "</a>";
+    navBar.appendChild(navElement);
+}
+
+//Scroll to section when Navbar link is clicked
+const link = document.querySelectorAll(".menu__link");
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener("click", (event) => {
+        event.preventDefault();
+        let index = i + 1;
+        let goto_section = document.getElementById("section" + index);
+        goto_section.scrollIntoView();
+    });
+}
+
+//Fucntion of adding active class
+function addActive() {
+    var addElement = document.querySelectorAll("section");
+    for (section of addElement) {
+        section.classList.add("your-active-class");
+    }
+}
+//function for removing active class
+function removeActive() {
+    var removeElement = document.querySelectorAll("section");
+    for (section of removeElement) {
+        section.classList.remove("your-active-class");
+    }
+}
+
+//Setting the area in view as active
+function makeActiveClass() {
+    for (section of sections)
+        if (section.getBoundingClienRect().top < window.innerHeight) {
+            addActive();
+        } else {
+            removeActive();
+        }
+}
+
+window.addEventListener("scroll", makeActiveClass());
---
+++ README.md
@@ -0,0 +1,21 @@
+# General Text
+This is the Landing Page Project README
+
+## Languages Used
+HTML
+CSS
+Javascript
+
+## Functionality
+Navigation Bar
+Dynamic Active Section
+Smooth Scrolling
+
+## Purpose
+Learning to convert a static landing page to an active landing page
+
+## How to Use This Project
+1-Open browser
+2-Select ctrl-O
+3-Navigate to the index.html file
+4-Open the index.html file in the browser (double click)
